6beff06bf9ab74ec6971d3dd4c88f6da51b54fd2
amosio <arkadiuszmosio@gmail>
2020-03-24 14:10:48 +0100

    after build

---
+++ build/precache-manifest.70721bc76f77b9b8c16f8bbcebd05f82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe2acc41aee0870e17d2a7109fd4d5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b1d1df43299f304e70",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b40ea6d00f1ce468c47c",
+    "url": "/static/js/2.3ef436c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ef436c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b1d1df43299f304e70",
+    "url": "/static/js/main.9ec80b10.chunk.js"
+  },
+  {
+    "revision": "c081a61104c9ff94d2d4",
+    "url": "/static/js/runtime-main.f16a4c84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
